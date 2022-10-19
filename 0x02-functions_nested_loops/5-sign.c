563c6dcd290bbe3e7748e07ee0ea0efa8cfa598d
Sarycode <abdulrahmanshopade@gmail.com>
2022-10-19 07:24:35 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_sign - a function that prints the sign of a number
+ *@n: sign on the number
+ *Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	} else if (n == 0)
+		return (0);
+		_putchar('0');
+	} else
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
