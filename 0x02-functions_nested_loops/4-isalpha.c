06fa2e18bbc80383d5c1b3c4f747d41b31158a81
Sarycode <abdulrahmanshopade@gmail.com>
2022-10-19 07:11:23 -0700

    _isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,11 @@
 /**
  *_isalpha - a function that checks for lowercase character
  *@c: single letter
- *Return: Return: 1 if int c is lowercase, 0 if otherwise
+ *Return: 1 if c is a letter (lower or uppercase), 0 otherwise
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
 		return (1);
 	else
 		return (0);
