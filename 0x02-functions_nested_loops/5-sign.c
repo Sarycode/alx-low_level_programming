b80262e25be152a1f29b2d42cc32834dd188c8fe
Sarycode <114301499+Sarycode@users.noreply.github.com>
2022-10-19 17:26:17 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *print_sign - a function that prints the sign of a number
- *@n: sign on the number
- *Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n < 0
+ * print_sign - a function that prints the sign of a number
+ * @n: number input
+ * Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n < 0
  */
 int print_sign(int n)
 {
@@ -12,6 +12,7 @@ int print_sign(int n)
 		return (1);
 	}
 	else if (n == 0)
+	{
 		_putchar ('0');
 		return (0);
 	}
