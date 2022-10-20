b3dd153c449e69c450531712e9299edc621fbdbf
Sarycode <abdulrahmanshopade@gmail.com>
2022-10-20 16:23:20 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isdigit - a function that checks for uppercase character
+ *@c: integer input
+ *Return: Returns 1 if c is a digit and Returns 0 otherwise
+ */
+int _isupper(int c)
+{
+	if ((c >= '0') && (c <= '9'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
