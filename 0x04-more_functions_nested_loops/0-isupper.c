ccae4c10170925b389516f6e45cbdf031721b243
Sarycode <abdulrahmanshopade@gmail.com>
2022-10-20 15:51:02 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_isupper - a function that checks for uppercase character
+ *@c: character input
+ *Return: Returns 1 if c is uppercase and Returns 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		_putchar(c);
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
